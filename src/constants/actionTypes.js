77e3e732d5f36dc216325bc1a3183aa548b9d7d8
Sharma Ravi <sharma@applaunch.io>
2024-08-19 23:45:56 +0530

    updating latest code

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,6 @@
-export const CREATE = 'CREATE';
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
-export const FETCH_ALL = 'FETCH_ALL';
-export const LIKE = 'LIKE';
+export const UPDATE = "UPDATE"
+export const CREATE = "CREATE"
+export const DELETE = "DELETE"
+export const FETCH_ALL = "FETCH_ALL"
+export const AUTH = "AUTH"
+export const LOGOUT = "LOGOUT"
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
-import { combineReducers } from 'redux';
-
-import posts from './posts';
-
-export const reducers = combineReducers({ posts });
+import {combineReducers} from "redux"
+import posts from "./posts"
+import auth from "./auth"
+export default combineReducers ({
+    posts,auth
+})
